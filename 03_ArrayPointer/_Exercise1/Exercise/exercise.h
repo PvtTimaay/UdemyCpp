330a0eff28e554c83e7988a4d1e86b56b20754ed
PvtTimaay <trip_tripper@gmx.net>
2023-10-22 14:45:30 +0200

    Message

--- 03_ArrayPointer/_Exercise1/Exercise/exercise.h
+++ 03_ArrayPointer/_Exercise1/Exercise/exercise.h
@@ -7,5 +7,5 @@
 double array_sum(const double *p_array, const std::size_t length);
 
 // Exercise 2
-//std::int32_t *array_constructor(const std::int32_t &value,
-//                                const std::size_t length);
+std::int32_t *array_constructor(const std::int32_t &value,
+                                const std::size_t length);
