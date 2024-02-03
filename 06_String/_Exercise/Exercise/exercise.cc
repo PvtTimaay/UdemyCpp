9bc3075b63de3ba072742bc42065978202df5190
PvtTimaay <trip_tripper@gmx.net>
2024-02-03 19:39:19 +0100

    mssge

--- 06_String/_Exercise/Exercise/exercise.cc
+++ 06_String/_Exercise/Exercise/exercise.cc
@@ -6,9 +6,24 @@
 // Exercise 1
 std::uint8_t hex_xor(const std::uint8_t hex1, const std::uint8_t hex2)
 {
+    return (hex1 ^ hex2);
 }
 
 // Exercise 2
 ByteArray hex_vector_xor(const ByteArray &vec1, const ByteArray &vec2)
 {
+    if (vec1.size() != vec2.size())
+    {
+        std::cerr << "Error ungleiche Laenge!" << std::endl;
+        return ByteArray{};
+    }
+
+    auto result = ByteArray(vec1.size(), 0x00);
+
+    for (size_t i = 0; i < vec1.size(); ++i)
+    {
+        result[i] = hex_xor(vec1[i], vec2[i]);
+    }
+
+    return result;
 }
