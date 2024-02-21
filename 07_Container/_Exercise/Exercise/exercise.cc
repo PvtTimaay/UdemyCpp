f9ac13978a8d4aebc6be33a2f81011e9e1960224
PvtTimaay <trip_tripper@gmx.net>
2024-02-21 21:24:15 +0100

    mssge

--- 07_Container/_Exercise/Exercise/exercise.cc
+++ 07_Container/_Exercise/Exercise/exercise.cc
@@ -1,12 +1,37 @@
-#include <iostream>
-
 #include "exercise.h"
+#include <iostream>
+#include <iterator>
+#include <string>
 
 // Exercise 2
 std::string_view get_oldest_friend(const Friends &friends)
 {
+    std::string_view tempFriend = "";
+    int tempAlter = 0;
+
+    for (const auto &[key, valhalla] : friends) //NOTE structured binding mit [] direkt entpacken
+    {
+        if (valhalla.first > tempAlter)
+        {
+            tempFriend = key;
+            tempAlter = valhalla.first;
+        }
+    }
+    return tempFriend;
 }
 
 std::string_view get_heaviest_friend(const Friends &friends)
 {
+    std::string_view tempFriend = "";
+    int tempHeavy = 0;
+
+    for (const auto &item : friends)
+    {
+        if (item.second.second > tempHeavy)
+        {
+            tempFriend = item.first;
+            tempHeavy = item.second.second;
+        }
+    }
+    return tempFriend;
 }
