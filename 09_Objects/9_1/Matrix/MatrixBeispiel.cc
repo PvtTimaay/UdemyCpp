23b24d0d3968c54bf29eb5a3fac5352b44c6392f
PvtTimaay <trip_tripper@gmx.net>
2024-08-25 15:12:30 +0200

    mssgeAgust

---
+++ 09_Objects/9_1/Matrix/MatrixBeispiel.cc
@@ -0,0 +1,66 @@
+/*#include <iostream>
+#include "Matrix.h"
+
+Matrix::Matrix()
+{
+    std::cout << "default constructor\n";
+}
+
+Matrix::Matrix(const double &A, const double &B, const double &C, const double &D)
+{
+    std::cout << "user constructor\n";
+    m_A = A;
+    m_B = B;
+    m_C = C;
+    m_D = D;
+}
+
+void Matrix::print_matrix() const
+{
+    std::cout << m_A << "\t" << m_B << std::endl;
+    std::cout << m_C << "\t" << m_D << std::endl;
+}
+
+double Matrix::get_A() const
+{
+    return m_A;
+}
+
+double Matrix::get_B() const
+{
+    return m_B;
+}
+
+double Matrix::get_C() const
+{
+    return m_C;
+}
+
+double Matrix::get_D() const
+{
+    return m_D;
+}
+
+void Matrix::set_A(const double &new_A)
+{
+    m_A = new_A;
+}
+
+void Matrix::set_B(const double &new_B)
+{
+    m_B = new_B;
+}
+
+void Matrix::set_C(const double &new_C)
+{
+    m_C = new_C;
+}
+
+void Matrix::set_D(const double &new_D)
+{
+    m_D = new_D;
+}
+
+
+
+*/
